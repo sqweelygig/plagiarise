c4a61949f81d2328f27d6a4cb2178d5548bb2c70
Andrew Lucas <sqweelygig@googlemail.com>
2019-11-10 22:48:07 +0000

    Refactored main entry to be a class

--- src/index.ts
+++ src/index.ts
@@ -3,73 +3,95 @@ import * as Bluebird from "bluebird";
 import * as Chalk from "chalk";
 import { fetchArticle as fetchWikipediaArticle } from "./wikipediaFetcher";
 
-function log(params: { error?: boolean; headline: string; detail?: string }) {
-	const outputFunction = params.error
-		? console.error.bind(console)
-		: console.log.bind(console);
-	const headline = params.error
-		? Chalk.red(params.headline)
-		: Chalk.blue(params.headline);
-	params.detail
-		? outputFunction(headline, params.detail)
-		: outputFunction(headline);
-}
+class Plagarise {
+	private static log(params: {
+		error?: boolean;
+		headline: string;
+		detail?: string;
+	}): void {
+		const outputFunction = params.error
+			? console.error.bind(console)
+			: console.log.bind(console);
+		const headline = params.error
+			? Chalk.red(params.headline)
+			: Chalk.blue(params.headline);
+		params.detail
+			? outputFunction(headline, params.detail)
+			: outputFunction(headline);
+	}
 
-async function start(): Promise<NodeJS.Timeout[]> {
-	log({ headline: "Hello World!" });
-	const trainingData = [];
-	let previousCount = 0;
-	const monitor = setInterval(() => {
-		if (trainingData.length !== previousCount) {
-			log({
-				detail: [
-					trainingData.length.toString(),
-					"articles,",
-					(trainingData.length - previousCount).toString(),
-					"new.",
-				].join(" "),
-				headline: "Article statistics:",
-			});
-			previousCount = trainingData.length;
-		}
-	}, 5000);
-	const updateRender = (render: string) => {
-		log({
+	private static updateRender(render: string): void {
+		Plagarise.log({
 			detail: render.split(/\n/, 1)[0],
 			headline: "Presenting article:",
 		});
-	};
-	const reportError = (error: Error) => {
-		log({
+	}
+
+	private static reportError(error: Error): void {
+		Plagarise.log({
 			detail: error.message,
 			error: true,
 			headline: "Oh no!",
 		});
-	};
-	const appendTrainingData = (newData: string) => {
-		log({
-			detail: newData.split(/\n/, 1)[0],
-			headline: "Learnt article:",
+	}
+
+	private readonly trainingData: string[] = [];
+	private previousCount = 0;
+	private readonly timeouts: NodeJS.Timeout[] = [];
+
+	public async start(): Promise<void> {
+		Plagarise.log({ headline: "Hello World!" });
+		this.startMonitor();
+		await Bluebird.props({
+			wikipedia: fetchWikipediaArticle({
+				appendTrainingData: this.makeDataAppender(),
+				header: "plagiarism",
+				reportError: Plagarise.reportError,
+				updateRender: Plagarise.updateRender,
+			}),
 		});
-		trainingData.push(newData);
-	};
-	await Bluebird.props({
-		wikipedia: fetchWikipediaArticle({
-			appendTrainingData,
-			header: "plagiarism",
-			reportError,
-			updateRender,
-		}),
-	});
-	log({
-		detail: [trainingData.length.toString(), "articles"].join(" "),
-		headline: "Final statistics",
-	});
-	return [monitor];
+	}
+
+	public stop(): void {
+		Plagarise.log({
+			detail: [this.trainingData.length.toString(), "articles"].join(" "),
+			headline: "Final statistics",
+		});
+		this.timeouts.forEach(clearInterval);
+	}
+
+	private startMonitor(): void {
+		const monitor = setInterval(this.makeMonitor(), 5000);
+		this.timeouts.push(monitor);
+	}
+
+	private makeMonitor(): () => void {
+		return () => {
+			if (this.trainingData.length !== this.previousCount) {
+				Plagarise.log({
+					detail: [
+						this.trainingData.length.toString(),
+						"articles,",
+						(this.trainingData.length - this.previousCount).toString(),
+						"new.",
+					].join(" "),
+					headline: "Article statistics:",
+				});
+				this.previousCount = this.trainingData.length;
+			}
+		};
+	}
+
+	private makeDataAppender(): (newData: string) => void {
+		return (newData: string) => {
+			Plagarise.log({
+				detail: newData.split(/\n/, 1)[0],
+				headline: "Learnt article:",
+			});
+			this.trainingData.push(newData);
+		};
+	}
 }
 
-function stop(timeout: NodeJS.Timeout[]): void {
-	timeout.forEach(clearInterval);
-}
-
-start().then(stop);
+const plagiarise = new Plagarise();
+plagiarise.start().then(plagiarise.stop);
