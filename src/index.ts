da719e40d9be69c4747d72dafecc44a3a23a13fe
Andrew Lucas <sqweelygig@googlemail.com>
2019-11-10 21:50:06 +0000

    setInterval for article monitoring and encapsulated log functionality

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,66 @@
 /* tslint:disable:no-console */
 import * as Bluebird from "bluebird";
 import * as Chalk from "chalk";
-import { compact } from "lodash";
 import { Wikipedia } from "./wikipedia";
 
+function log(params: { error?: boolean; headline: string; detail?: string }) {
+	const outputFunction = params.error
+		? console.error.bind(console)
+		: console.log.bind(console);
+	const headline = params.error
+		? Chalk.red(params.headline)
+		: Chalk.blue(params.headline);
+	outputFunction(headline, params.detail);
+}
+
 async function start(): Promise<void> {
-	console.log(Chalk.blue("Hello World!"));
-	const plagiarism = await Bluebird.props({
+	log({ headline: "Hello World!" });
+	const trainingData = [];
+	let previousCount = 0;
+	const monitor = setInterval(() => {
+		if (trainingData.length !== previousCount) {
+			log({
+				detail: [
+					trainingData.length.toString(),
+					"articles,",
+					(trainingData.length - previousCount).toString(),
+					"new.",
+				].join(" "),
+				headline: "Article statistics:",
+			});
+			previousCount = trainingData.length;
+		}
+	}, 5000);
+	const plagiarismArticles = await Bluebird.props({
 		wikipedia: Wikipedia.fetchArticle("plagiarism"),
 	});
-	console.log(
-		Chalk.blue("Presenting article:"),
-		plagiarism.wikipedia.render.split(/\n/, 1)[0],
+	log({
+		detail: plagiarismArticles.wikipedia.render.split(/\n/, 1)[0],
+		headline: "Presenting article:",
+	});
+	await Bluebird.each(
+		plagiarismArticles.wikipedia.trainingData.fetchers,
+		async (fetcher) => {
+			try {
+				const article = await fetcher();
+				log({
+					detail: article.split(/\n/, 1)[0],
+					headline: "Learnt article:",
+				});
+				trainingData.push(article);
+			} catch (error) {
+				log({
+					detail: error.message,
+					error: true,
+					headline: "Oh no!",
+				});
+			}
+		},
 	);
-	const trainingData = compact(
-		await Bluebird.map(
-			plagiarism.wikipedia.trainingData.fetchers,
-			async (fetcher) => {
-				try {
-					const article = await fetcher();
-					console.log(Chalk.blue("Learnt article:"), article.split(/\n/, 1)[0]);
-					return article;
-				} catch (error) {
-					console.error(Chalk.red("Oh no!"), error.message);
-					return null;
-				}
-			},
-			{
-				concurrency: plagiarism.wikipedia.trainingData.concurrency,
-			},
-		),
-	);
-	console.log(Chalk.blue("Article count:"), trainingData.length.toString());
+	log({
+		detail: [trainingData.length.toString(), "articles"].join(" "),
+		headline: "Final statistics",
+	});
 }
 start().then(() => {
 	/* do nothing */
