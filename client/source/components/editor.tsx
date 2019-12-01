32009d7bf23bfaff6f678b83336da7540aa8d7e3
Andrew Lucas <sqweelygig@googlemail.com>
2019-12-01 16:33:10 +0000

    Moved brain updating function into brain class

--- client/source/components/editor.tsx
+++ client/source/components/editor.tsx
@@ -5,12 +5,7 @@ import { Logo } from "../elements/logo";
 import { Marginalia, MarginaliaProps } from "../elements/marginalia";
 import { TextTools } from "../elements/text-tools";
 import { TipTools } from "../elements/tip-tools";
-import {
-	Brain,
-	BrainEntry,
-	BrainValues,
-	BrainWriterFunctions,
-} from "../models/brain";
+import { Brain, BrainValues } from "../models/brain";
 import { fetch as fetchFortune } from "../models/fortune-cookie";
 
 type EditorState = EditPaneValues & MarginaliaProps & BrainValues;
@@ -26,7 +21,7 @@ export class Editor extends React.Component<{}, EditorState> {
 	}
 
 	public async componentDidMount(): Promise<void> {
-		const update = this.encloseBrainWriters("fortune cookies");
+		const update = Brain.encloseBrainWriters(this, "fortune cookies");
 		await fetchFortune(update);
 		await fetchFortune(update);
 	}
@@ -39,7 +34,7 @@ export class Editor extends React.Component<{}, EditorState> {
 				editorTimeout={this.state.editorTimeout}
 				editorValue={this.state.editorValue}
 				onChange={this.setState.bind(this)}
-				writeToBrain={this.encloseBrainWriters("main editor")}
+				writeToBrain={Brain.encloseBrainWriters(this, "main editor")}
 				key="editor_pane"
 			/>,
 			<Marginalia
@@ -50,26 +45,5 @@ export class Editor extends React.Component<{}, EditorState> {
 			<TextTools key="text_tools" />,
 			<TipTools key="tip-tools" />,
 		];
-	}
-
-	private encloseBrainWriters(source: string): BrainWriterFunctions {
-		const update = (entry: BrainEntry, index?: number) => {
-			this.setState((oldState) => {
-				const brainEntries = oldState.brainEntries.concat();
-				const end = brainEntries.length;
-				// If we were given an index, and that entry is ours, then use the index, otherwise add to the end
-				const insert =
-					index && brainEntries[index].source === source ? index : end;
-				brainEntries[insert] = merge({ source }, entry);
-				return { brainEntries };
-			});
-			// TODO This doesn't respect that setState might be compounded
-			const state = this.state;
-			const length = state.brainEntries.length;
-			return index && state.brainEntries[index].source === source
-				? index
-				: length;
-		};
-		return { update };
 	}
 }
--- client/source/models/brain.ts
+++ client/source/models/brain.ts
@@ -1,3 +1,6 @@
+import { merge } from "lodash";
+import * as React from "react";
+
 export interface BrainValues {
 	brainEntries: BrainCitedEntry[];
 }
@@ -29,6 +32,27 @@ export type SimpleBrainWriter = (
 ) => Promise<void>;
 
 export class Brain {
+	public static encloseBrainWriters(
+		stateStore: React.Component<any, BrainValues>,
+		source: string,
+	): BrainWriterFunctions {
+		const update = (entry: BrainEntry, index?: number) => {
+			stateStore.setState((oldState) => {
+				const entries = oldState.brainEntries.concat();
+				const end = entries.length;
+				// If we were given an index, and that entry is ours, then use the index, otherwise add to the end
+				const insert = index && entries[index].source === source ? index : end;
+				entries[insert] = merge({ source }, entry);
+				return { brainEntries: entries };
+			});
+			// TODO This doesn't respect that setState might be compounded, and should
+			const ents = stateStore.state.brainEntries;
+			const length = stateStore.state.brainEntries.length;
+			return index && ents[index].source === source ? index : length;
+		};
+		return { update };
+	}
+
 	public static createEmpty(): BrainCitedEntry[] {
 		return [];
 	}
